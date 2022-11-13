d45b25973bacfd87e3815d12f2c6654c0709b336
Evi7princ8 <Sdikshith7885@gmail.com>
2022-11-13 23:00:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const apiKey = "ca4455137c6f9de08bd8869e1d154a00";
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+
+  const getWeather = (event) => {
+    if (event.key == "Enter") {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data);
+        });
+    }
+  };
+  return (
+    <div className="container">
+      <input
+        className="input"
+        placeholder="Enter City..."
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather}
+      />
+
+      {typeof weatherData.main === "undefined" ? (
+        <div>
+          <p>Welcome to weather app! Enter the city to get the weather of.</p>
+        </div>
+      ) : (
+        <div className="weather-data">
+          <p className="city">{weatherData.name}</p>
+          <p className="temp">{Math.round(weatherData.main.temp)}°F</p>
+          <p className="weather">{weatherData.weather[0].main}</p>
+        </div>
+      )}
+
+      {weatherData.cod === "404" ? <p>City not found</p> : <></>}
+    </div>
+  );
+}
+
+export default App;
